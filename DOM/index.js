21b6001add6decded1cb41dd5bce5d642c21c60a
rudra pratap singh <prataprudra10102001@gmail.com>
2024-12-18 01:14:06 +0530

    input

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,23 @@
-document.query
+let third = document.querySelector('.list');
+third.innerHTML="its worked"
+
+
+//tag name
+let tag_name=document.getElementsByTagName("li")[2];
+tag_name.innerHTML="work with tagName";
+
+//class name
+let Class_name=document.getElementsByClassName("list")[1];
+Class_name.style.color="red";
+
+
+//id name
+let eleId=document.getElementById("Class1");
+eleId.style.backgroundColor="green";
+
+
+//query selector
+let=query_name=document.querySelector("p");
+query_name.style.color="red";
+
+
---
+++ DOM/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+
+let Class_name=document.getElementsByClassName("list")[1];
+Class_name.computedStyleMap.color=red;
+alert(Class_name)
