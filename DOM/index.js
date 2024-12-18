b97a25ac9d137073fe2b978f56d6e6c1dc21ee22
rudra pratap singh <prataprudra10102001@gmail.com>
2024-12-18 15:42:15 +0530

    attributes

--- DOM/index.js
+++ DOM/index.js
@@ -1,74 +1,94 @@
-// // // // // // let third = document.querySelector('.list');
-// // // // // // third.innerHTML="its worked"
+// // // // // // // // let third = document.querySelector('.list');
+// // // // // // // // third.innerHTML="its worked"
 
-// // // // // Input types in javascript
-
-
-// // // // // // //tag name
-// // // // // // let tag_name=document.getElementsByTagName("li")[2];
-// // // // // // tag_name.innerHTML="work with tagName";
-
-// // // // // // //class name
-// // // // // // let Class_name=document.getElementsByClassName("list")[1];
-// // // // // // Class_name.style.color="red";
-
-// // // // // // //id name
-// // // // // // let eleId=document.getElementById("Class1");
-// // // // // // eleId.style.backgroundColor="green";
-
-// // // // // // //query selector
-// // // // // // let=query_name=document.querySelector("p");
-// // // // // // query_name.style.color="red";
-
-// // // // // let color_a=document.querySelector("li a")
-// // // // // color_a.style.color="yellow"
+// // // // // // // Input types in javascript
 
 
-// // // // //CSS styling and changing using Java  Script
+// // // // // // // // //tag name
+// // // // // // // // let tag_name=document.getElementsByTagName("li")[2];
+// // // // // // // // tag_name.innerHTML="work with tagName";
 
-// // // // //changing color
-// // // // let color_change=document.querySelector("li a");
-// // // // color_change.style.backgroundColor="red";
-// // // // color_change.style.fontSize="140px";
+// // // // // // // // //class name
+// // // // // // // // let Class_name=document.getElementsByClassName("list")[1];
+// // // // // // // // Class_name.style.color="red";
 
+// // // // // // // // //id name
+// // // // // // // // let eleId=document.getElementById("Class1");
+// // // // // // // // eleId.style.backgroundColor="green";
 
-// // // // //hiding css
+// // // // // // // // //query selector
+// // // // // // // // let=query_name=document.querySelector("p");
+// // // // // // // // query_name.style.color="red";
 
-// // // // color_change.style.visibility="hidden";
-
-
-// // // document.querySelector("button").style.backgroundColor="green"
-
-
-// // //classList
-
-// // //getting class using js
-// // let class_list=document.querySelector("button").classList;
-// // alert(class_list)
-
-// // //adding class list
-
-// // class_list.add("class2");
-// // alert(class_list)
+// // // // // // // let color_a=document.querySelector("li a")
+// // // // // // // color_a.style.color="yellow"
 
 
-// // //remove 
+// // // // // // //CSS styling and changing using Java  Script
 
-// // class_list.remove("class2");
-// // alert(class_list)
-
-// // //toggle the class
-
-// // class_list.toggle("class2");
-// // alert(class_list)
+// // // // // // //changing color
+// // // // // // let color_change=document.querySelector("li a");
+// // // // // // color_change.style.backgroundColor="red";
+// // // // // // color_change.style.fontSize="140px";
 
 
-// document.querySelector("h1").classList.add("huge");
-// document.querySelector("h1").classList.remove("huge");
-// document.querySelector("h1").classList.toggle("huge");
+// // // // // // //hiding css
+
+// // // // // // color_change.style.visibility="hidden";
 
 
-//textContent
+// // // // // document.querySelector("button").style.backgroundColor="green"
 
-document.querySelector("h2").textContent="BRO"
-document.querySelector("h2").innerHTML="innerHtml"
+
+// // // // //classList
+
+// // // // //getting class using js
+// // // // let class_list=document.querySelector("button").classList;
+// // // // alert(class_list)
+
+// // // // //adding class list
+
+// // // // class_list.add("class2");
+// // // // alert(class_list)
+
+
+// // // // //remove 
+
+// // // // class_list.remove("class2");
+// // // // alert(class_list)
+
+// // // // //toggle the class
+
+// // // // class_list.toggle("class2");
+// // // // alert(class_list)
+
+
+// // // document.querySelector("h1").classList.add("huge");
+// // // document.querySelector("h1").classList.remove("huge");
+// // // document.querySelector("h1").classList.toggle("huge");
+
+
+// // //textContent
+
+// // document.querySelector("h2").textContent="BRO"
+// // document.querySelector("h2").innerHTML="innerHtml"
+
+
+
+// let hrf=document.querySelector("a");
+// hrf.attributes;
+// alert(hrf);
+
+
+// //getAttribute
+// hrf.getAttribute("href");
+// alert(hrf);
+
+// //setting attribute
+
+// hrf.setAttribute("href","http://192.168.2.100/");
+// alert(hrf);
+
+
+
+
