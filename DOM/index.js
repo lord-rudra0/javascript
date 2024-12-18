9e0dcffb22d470c9bb37ebdc2e50b92ce7605e70
rudra pratap singh <prataprudra10102001@gmail.com>
2024-12-18 15:10:21 +0530

    js

--- DOM/index.js
+++ DOM/index.js
@@ -1,23 +1,45 @@
-let third = document.querySelector('.list');
-third.innerHTML="its worked"
+// // // // let third = document.querySelector('.list');
+// // // // third.innerHTML="its worked"
+
+// // // Input types in javascript
 
 
-//tag name
-let tag_name=document.getElementsByTagName("li")[2];
-tag_name.innerHTML="work with tagName";
+// // // // //tag name
+// // // // let tag_name=document.getElementsByTagName("li")[2];
+// // // // tag_name.innerHTML="work with tagName";
 
-//class name
-let Class_name=document.getElementsByClassName("list")[1];
-Class_name.style.color="red";
+// // // // //class name
+// // // // let Class_name=document.getElementsByClassName("list")[1];
+// // // // Class_name.style.color="red";
+
+// // // // //id name
+// // // // let eleId=document.getElementById("Class1");
+// // // // eleId.style.backgroundColor="green";
+
+// // // // //query selector
+// // // // let=query_name=document.querySelector("p");
+// // // // query_name.style.color="red";
+
+// // // let color_a=document.querySelector("li a")
+// // // color_a.style.color="yellow"
 
 
-//id name
-let eleId=document.getElementById("Class1");
-eleId.style.backgroundColor="green";
+// // //CSS styling and changing using Java  Script
+
+// // //changing color
+// // let color_change=document.querySelector("li a");
+// // color_change.style.backgroundColor="red";
+// // color_change.style.fontSize="140px";
 
 
-//query selector
-let=query_name=document.querySelector("p");
-query_name.style.color="red";
+// // //hiding css
+
+// // color_change.style.visibility="hidden";
+
+
+// document.querySelector("button").style.backgroundColor="green"
+
+
+
 
 
