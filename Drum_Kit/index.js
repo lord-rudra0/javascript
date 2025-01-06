e09c7fb9639a3536df73d88f879b280b427843fa
rudra pratap singh <prataprudra10102001@gmail.com>
2025-01-06 19:38:40 +0530

    object

--- Drum_Kit/index.js
+++ Drum_Kit/index.js
@@ -1,18 +1,18 @@
-w_button=document.querySelector(".w");
-a_button=document.querySelector(".a");
-s_button=document.querySelector(".s");
-d_button=document.querySelector(".d");
-j_button=document.querySelector(".j");
-k_button=document.querySelector(".k");
-l_button=document.querySelector(".l");
+// w_button=document.querySelector(".w");
+// a_button=document.querySelector(".a");
+// s_button=document.querySelector(".s");
+// d_button=document.querySelector(".d");
+// j_button=document.querySelector(".j");
+// k_button=document.querySelector(".k");
+// l_button=document.querySelector(".l");
 
-w_button.addEventListener("click",click_music)
+// w_button.addEventListener("click",click_music)
 
-// drum=document.querySelectorAll(".drum")
-// for(var i=0;i<drum.length;i++)
-// {
-// drum[i].addEventListener("click",click_music);
-// }
+drum=document.querySelectorAll(".drum")
+for(var i=0;i<drum.length;i++)
+{
+this.drum[i].addEventListener("click",click_music);
+}
 
 
 function click_music()
---
+++ JS_Object/index.js
@@ -0,0 +1,13 @@
+var bel_boy1={
+    name:"abc",
+    age:22,
+    languages:["Hindi","English"]
+}
+
+var housekeeper1={
+    name:"abc",
+    age:22,
+    worked:["A","B","C"],
+    exp:12
+
+}
