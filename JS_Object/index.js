dd323532dcfd6140b2f99348343745fd97a18bf1
rudra pratap singh <prataprudra10102001@gmail.com>
2025-01-06 19:55:15 +0530

    new

--- JS_Object/index.js
+++ JS_Object/index.js
@@ -11,3 +11,14 @@ var housekeeper1={
     exp:12
 
 }
+
+// constructor Function
+
+function BellBoy(name,age,languages)
+{
+this.name=name;
+this.age=age;
+this.languages=languages;
+}
+
+var BellBoy3=new BellBoy("ABC",22,["Hindi","English"]);
